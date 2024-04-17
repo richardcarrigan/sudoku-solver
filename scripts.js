1c37a804dee7483cb979a7ad993da5a6d2285db1
Richard Carrigan <ricarrigan@actalentservices.com>
2024-04-17 15:38:47 -0700

    Adds all cell values into 2D array

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
 function solvePuzzle() {
+  let values = [];
+  const puzzle = document.getElementById('puzzle');
+  const boxes = puzzle.querySelectorAll('.box');
+  boxes.forEach(box => {
+    let boxValues = [];
+    const cells = box.querySelectorAll('.cell');
+    cells.forEach(cell => {
+      boxValues.push(cell.value);
+    });
+    values.push(boxValues);
+  });
   console.log('Solved!');
+  console.log(values);
 }
